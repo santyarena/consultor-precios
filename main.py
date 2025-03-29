7917c0998159da24452db342630f84d6ac953370
santyarena <111534673+santyarena@users.noreply.github.com>
2025-03-28 22:30:16 -0300

    main.py

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from flask import Flask, request, jsonify
+import os
 
 app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "API funcionando correctamente."
 
 @app.route("/consultar", methods=["POST"])
 def consultar():
     data = request.get_json()
-    producto = data.get("producto", "Desconocido")
+    producto = data.get("producto", "").lower()
+
+    # Simulación de precios
+    precios_simulados = {
+        "5600g": "$134.999",
+        "5700g": "$144.999",
+        "5800x": "$164.999"
+    }
+
+    precio = precios_simulados.get(producto, "Producto no encontrado")
+
     return jsonify({
         "producto": producto,
-        "precio": "$123456"  # Simulado por ahora
+        "precio": precio
     })
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=3000)
-
+    port = int(os.environ.get("PORT", 3000))
+    app.run(debug=False, host="0.0.0.0", port=port)
